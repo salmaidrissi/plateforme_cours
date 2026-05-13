1bdd188a7d0feee03065d7fb86e38599c5eacd99
Salma Idrissi <169618279+salmaidrissi@users.noreply.github.com>
2026-05-13 19:58:35 +0100

    ajout de crud

--- cours/models.py
+++ cours/models.py
@@ -1,20 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Cours(models.Model):
+
     titre = models.CharField(max_length=200)
+
     description = models.TextField()
-    enseignant = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    image = models.ImageField(
+        upload_to='cours_images/',
+        null=True,
+        blank=True
+    )
+
+    enseignant = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.titre
 
 
 class Inscription(models.Model):
-    etudiant = models.ForeignKey(User, on_delete=models.CASCADE)
-    cours = models.ForeignKey(Cours, on_delete=models.CASCADE)
-    date_inscription = models.DateTimeField(auto_now_add=True)
+
+    etudiant = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    cours = models.ForeignKey(
+        Cours,
+        on_delete=models.CASCADE
+    )
+
+    date_inscription = models.DateTimeField(
+        auto_now_add=True
+    )
 
     def __str__(self):
-        return f"{self.etudiant.username} - {self.cours.titre}"
 
+        return f"{self.etudiant.username} - {self.cours.titre}"
--- cours/views.py
+++ cours/views.py
@@ -1,6 +1,80 @@
 from django.shortcuts import render
 from .models import Cours
+from .forms import CoursForm
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 def liste_cours(request):
     cours = Cours.objects.all()
     return render(request, 'cours/liste.html', {'cours': cours})
+
+def ajouter_cours(request):
+
+    if request.method == 'POST':
+
+        form = CoursForm(
+            request.POST,
+            request.FILES
+        )
+
+        if form.is_valid():
+
+            cours = form.save(commit=False)
+
+            cours.enseignant = User.objects.first()
+
+            cours.save()
+
+            return redirect('liste_cours')
+
+    else:
+
+        form = CoursForm()
+
+    return render(
+        request,
+        'cours/ajouter.html',
+        {'form': form}
+    )
+def modifier_cours(request, cours_id):
+
+    cours = get_object_or_404(Cours, id=cours_id)
+
+    if request.method == 'POST':
+
+        form = CoursForm(
+            request.POST,
+            request.FILES,
+            instance=cours
+        )
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('liste_cours')
+
+    else:
+
+        form = CoursForm(instance=cours)
+
+    return render(
+        request,
+        'cours/modifier.html',
+        {'form': form}
+    )
+
+def supprimer_cours(request, cours_id):
+
+    cours = get_object_or_404(Cours, id=cours_id)
+
+    cours.delete()
+
+    return redirect('liste_cours')
+
+def detail_cours(request, cours_id):
+
+    cours = get_object_or_404(Cours, id=cours_id)
+
+    return render(request, 'cours/detail.html', {'cours': cours})
